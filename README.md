af82a03e05b64dccf655ab159819eb1573de987e
RouxRC <b.ooghe@gmail.com>
2016-01-02 06:08:18 +0100

    first commit with script

---
+++ README.md
@@ -0,0 +1,4 @@
+# TI-IntegrityWatch-data
+
+Versioning des données parlementaires présentées sur le site [IntegrityWatch.fr](http://www.integritywatch.fr) de [Transparency International France](http://www.transparency-france.org/) à partir des déclarations d'intérêts des parlementaires.
+
---
+++ bin/update.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd $(dirname $0)/..
+
+mkdir -p data
+
+git pull
+
+function updateCsv {
+  sourcefile=$1
+  outfile=$2
+  curl -sfL "http://www.integritywatch.fr/${sourcefile}.csv" > "data/${outfile}.tmp"
+  if test -s "data/${outfile}.tmp"; then
+    mv -f "data/${outfile}."{tmp,csv}
+  fi
+}
+
+updateCsv parlementaires parlementaires
+updateCsv collaborators collaborateurs
+updateCsv p_details interets
+
+if git status | grep "data" > /dev/null; then
+  git commit data -m "autoupdate"
+  git push
+fi
